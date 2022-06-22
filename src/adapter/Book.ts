92a8be693caa7de6d041cd24c20a7aa7ca43f0cf
Syahril Syahputra <arhiel@Syahrils-iMac.local>
2022-06-22 16:06:18 +0800

    finish

--- src/adapter/Book.ts
+++ src/adapter/Book.ts
@@ -3,7 +3,6 @@ interface IBook {
   penulis: string;
   tglTerbit: Date;
   harga: number;
-  
 }
 
 class Book implements IBook {
@@ -12,11 +11,42 @@ class Book implements IBook {
   public tglTerbit: Date;
   public harga: number;
 
+  public getJudul(): string {
+    return this.judul;
+  }
+
+  public setJudul(judul: string): void {
+    this.judul = judul;
+  }
+
+  public getPenulis(): string {
+    return this.penulis;
+  }
+
+  public setPenulis(penulis: string): void {
+    this.penulis = penulis;
+  }
+
+  public getTglTerbit(): Date {
+    return this.tglTerbit;
+  }
+
+  public setTglTerbit(tglTerbit: Date): void {
+    this.tglTerbit = tglTerbit;
+  }
+
+  public getHarga(): number {
+    return this.harga;
+  }
+
+  public setHarga(harga: number): void {
+    this.harga = harga;
+  }
+
   constructor(judul: string, penulis: string, tglTerbit: Date, harga: number) {
     this.judul = judul;
     this.penulis = penulis;
     this.tglTerbit = tglTerbit;
     this.harga = harga;
   }
-
 }
--- src/adapter/Music.ts
+++ src/adapter/Music.ts
@@ -4,3 +4,60 @@ interface IMusic{
     harga : number,
     tglRelease : Date
 }
+
+class Music implements IMusic{
+    public judul: string;
+    public penyanti: string;
+    public harga: number;
+    public tglRelease: Date;
+
+    public getJudul(): string {
+        return this.judul;
+    }
+
+    public setJudul(judul: string): void {
+        this.judul = judul;
+    }
+
+    public getPenyanti(): string {
+        return this.penyanti;
+    }
+
+    public setPenyanti(penyanti: string): void {
+        this.penyanti = penyanti;
+    }
+
+    public getHarga(): number {
+        return this.harga;
+    }
+
+    public setHarga(harga: number): void {
+        this.harga = harga;
+    }
+
+    public getTglRelease(): Date {
+        return this.tglRelease;
+    }
+
+    public setTglRelease(tglRelease: Date): void {
+        this.tglRelease = tglRelease;
+    }
+
+
+  constructor(
+    judul: string, 
+    penyanti: string, 
+    harga: number, 
+    tglRelease: Date
+) {
+    this.judul = judul
+    this.penyanti = penyanti
+    this.harga = harga
+    this.tglRelease = tglRelease
+  }
+
+
+
+
+    
+}
